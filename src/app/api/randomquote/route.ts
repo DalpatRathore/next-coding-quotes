64e80e3703d8a7cead122dbbd8fe38e98f47c24c
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2023-07-22 20:53:17 +0530

    Project complete

---
+++ src/app/api/randomquote/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+
+import getRandomQuote from "@/lib/getRandomQuote";
+
+export async function GET(request: Request) {
+  const quote = await getRandomQuote();
+
+  return NextResponse.json(quote);
+}
