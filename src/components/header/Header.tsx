e4ddd82f1d3212b4a93582936120b35e062c77ad
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-06-03 12:53:20 +0530

    feat: repo github link

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,23 @@
 import Image from "next/image";
 import ThemeToggle from "../ThemeToggle";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
 const Header = () => {
   return (
     <header className="flex items-center justify-between p-5 max-w-[1920px] mx-auto">
-      <div className="logo">
+      <div className="w-16 h-16 md:w-24 md:h-24 flex items-center justify-center">
         <Image src={"/logo.svg"} width={100} height={100} alt="logo"></Image>
+      </div>
+      <div className="p-1 w-fit rounded-full border">
+        <a
+          href="https://github.com/DalpatRathore/next-coding-quotes"
+          target="_blank"
+        >
+          <Avatar className="w-10 h-10 md:w-16 md:h-16 shadow-sm shadow-black">
+            <AvatarImage src="/dalpatrathore.png"></AvatarImage>
+            <AvatarFallback>D R</AvatarFallback>
+          </Avatar>
+        </a>
       </div>
       <ThemeToggle></ThemeToggle>
     </header>
