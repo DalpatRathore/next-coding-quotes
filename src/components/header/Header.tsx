2908fdce4fefea9c32cb07f35b9a3e633e186c80
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-06-10 18:48:32 +0530

    style: header icons

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,8 +4,8 @@ import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between p-5 max-w-[1920px] mx-auto">
-      <div className="w-16 h-16 md:w-24 md:h-24 flex items-center justify-center">
+    <header className="flex items-center justify-between px-5 max-w-[1920px] mx-auto">
+      <div className="w-16 h-16 md:w-20 md:h-20 flex items-center justify-center">
         <Image src={"/logo.svg"} width={100} height={100} alt="logo"></Image>
       </div>
       <div className="p-1 w-fit rounded-full border">
@@ -13,7 +13,7 @@ const Header = () => {
           href="https://github.com/DalpatRathore/next-coding-quotes"
           target="_blank"
         >
-          <Avatar className="w-10 h-10 md:w-16 md:h-16 shadow-sm shadow-black">
+          <Avatar className="w-10 h-10 md:w-12 md:h-12 shadow-sm shadow-black">
             <AvatarImage src="/dalpatrathore.png"></AvatarImage>
             <AvatarFallback>D R</AvatarFallback>
           </Avatar>
