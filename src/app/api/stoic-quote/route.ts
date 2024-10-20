d1a677bab0157b79c636761c4ac3bd70ae627dff
DalpatRathore <69510006+DalpatRathore@users.noreply.github.com>
2024-10-20 23:11:02 +0530

    fix: dynamic server usage issue

--- src/app/api/stoic-quote/route.ts
+++ src/app/api/stoic-quote/route.ts
@@ -1,10 +1,9 @@
 import { NextResponse } from "next/server";
+export const dynamic = 'force-dynamic' 
 
 export async function GET() {
   try {
-    const response = await fetch("https://stoic.tekloon.net/stoic-quote", {
-      cache: 'no-store', // This ensures the request isn't cached
-    });
+    const response = await fetch("https://stoic.tekloon.net/stoic-quote",{ cache: 'no-store' });
     const data = await response.json();
     
     return NextResponse.json(data);
